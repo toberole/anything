5b5085d2a8aab3d2f1fa023876a38faf1abf121a
zhouwei <zhouwei11@58ganji.com>
2017-12-13 10:14:43 +0800

    截图

--- app/src/main/java/com/zhouwei/anything/activity/AnimatorActivity.java
+++ app/src/main/java/com/zhouwei/anything/activity/AnimatorActivity.java
@@ -1,12 +1,15 @@
 package com.zhouwei.anything.activity;
 
 import android.animation.ValueAnimator;
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.zhouwei.anything.R;
@@ -17,11 +20,15 @@ public class AnimatorActivity extends AppCompatActivity {
     private Button btntar;
     private Button btnani;
     private int height;
+    private Button btncaptureScreen;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animator);
+        this.iv = (ImageView) findViewById(R.id.iv);
+        this.btncaptureScreen = (Button) findViewById(R.id.btn_captureScreen);
         this.btnani = (Button) findViewById(R.id.btn_ani);
         this.btntar = (Button) findViewById(R.id.btn_tar);
         this.btngo = (Button) findViewById(R.id.btn_go);
@@ -34,6 +41,21 @@ public class AnimatorActivity extends AppCompatActivity {
                 animator2();
             }
         });
+
+        btncaptureScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = captureScreen(AnimatorActivity.this);
+                iv.setImageBitmap(bitmap);
+            }
+        });
+    }
+
+    public static Bitmap captureScreen(Activity activity) {
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        Bitmap bmp = view.getDrawingCache();
+        return bmp;
     }
 
     private void animator1(View v) {
