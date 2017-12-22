98cbc1e54b1d23ceb693738ca298e9ec76a9f0d1
zhouwei <zhouwei11@58ganji.com>
2017-12-22 13:48:36 +0800

    Revert "XXORM version 1"
    This reverts commit cda3c5ad0bb65dae5415de65894dc9f8b790cb7e.
    
    # Conflicts:
    #	app/src/main/assets/db.xml
    #	app/src/main/java/com/zhouwei/anything/activity/MainActivity.java
    #	app/src/main/java/com/zhouwei/anything/bean/Person.java
    #	app/src/main/java/com/zhouwei/anything/bean/Student.java
    #	app/src/main/java/com/zhouwei/anything/xxorm/DBConfig.java
    #	app/src/main/java/com/zhouwei/anything/xxorm/DBEngine.java
    #	app/src/main/java/com/zhouwei/anything/xxorm/Property.java
    #	app/src/main/java/com/zhouwei/anything/xxorm/Table.java
    #	app/src/main/java/com/zhouwei/anything/xxorm/XMLParser.java

--- app/src/main/java/com/zhouwei/anything/activity/MainActivity.java
+++ app/src/main/java/com/zhouwei/anything/activity/MainActivity.java
@@ -1,137 +1,45 @@
 package com.zhouwei.anything.activity;
 
 import android.app.Activity;
-import android.content.ContentValues;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.zhouwei.anything.R;
-import com.zhouwei.anything.bean.Person;
-import com.zhouwei.anything.xxorm.DBEngine;
 
-import java.util.List;
-
-
-public class MainActivity extends Activity implements View.OnClickListener {
-
-    private android.widget.Button add;
-    private android.widget.Button delete;
-    private android.widget.Button update;
-    private android.widget.Button get;
-    private Button getPart;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.getPart = (Button) findViewById(R.id.getPart);
-        this.get = (Button) findViewById(R.id.get);
-        this.update = (Button) findViewById(R.id.update);
-        this.delete = (Button) findViewById(R.id.delete);
-        this.add = (Button) findViewById(R.id.add);
-
-
-        get.setOnClickListener(this);
-        add.setOnClickListener(this);
-        delete.setOnClickListener(this);
-        update.setOnClickListener(this);
-        getPart.setOnClickListener(this);
-
         ViewGroup decorView = (ViewGroup) getWindow().getDecorView();
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-
-        if (id == R.id.add) {
-            add();
-        } else if (id == R.id.get) {
-            get();
-        } else if (id == R.id.delete) {
-            delete();
-        } else if (id == R.id.update) {
-            update();
-        } else if (id == R.id.getPart) {
-            getPart();
-        }
-    }
-
-    private void add() {
-        Log.i("AAAA", "==add==");
-        Person person = new Person();
-        person.setAge(11);
-        person.setName("xiaohon");
-        DBEngine.getInstance().save(person);
-
-        ContentValues values = new ContentValues();
-        values.put("tag", "MainActivity");
-        values.put("msg", " activity onCreate");
-        values.put("time", System.currentTimeMillis());
-        values.put("thread", Thread.currentThread().getName());
-        com.zhouwei.anything.bean.Log log = new com.zhouwei.anything.bean.Log(values);
-        DBEngine.getInstance().save(log);
-    }
-
-    private void get() {
-        Log.i("AAAA", "==get==");
-        List<Person> ps = DBEngine.getInstance().get(Person.class, null, null, null, null, null);
-        if (null != ps) {
-            for (int i = 0; i < ps.size(); i++) {
-                Log.i("AAAA", "person: " + ps.get(i));
-            }
-        }
-
-        List<com.zhouwei.anything.bean.Log> logs = DBEngine.getInstance().get(com.zhouwei.anything.bean.Log.class, null, null, null, null, null);
-        for (int i = 0; i < logs.size(); i++) {
-            Log.i("AAAA", logs.get(i) + "");
-        }
-    }
-
-    private void getPart() {
-        List<Person> ps = DBEngine.getInstance().get(Person.class, 15, 3);
-        if (null != ps) {
-            for (int i = 0; i < ps.size(); i++) {
-                Log.i("AAAA", "person: " + ps.get(i));
-            }
-        }
-    }
-
-    private void delete() {
-        Log.i("AAAA", "==delete==");
-        DBEngine.getInstance().delete(Person.class, null, null);
-    }
-
-    private void update() {
-        Log.i("AAAA", "==update==");
-        ContentValues values = new ContentValues();
-        values.put("name", "XXORM");
-        String whereClause = "age = ?";
-        String[] whereArgs = new String[]{11 + ""};
-        DBEngine.getInstance().update(Person.class, values, whereClause, whereArgs);
+        Log.i("AAAA", "decorView: " + decorView.getChildCount());
+        Log.i("AAAA", "decorView child: " + decorView.getChildAt(0));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.i("AAAA", "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.i("AAAA", "onResume");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Log.i("AAAA", "onRestart");
     }
 
     @Override
     public void setVisible(boolean visible) {
         super.setVisible(visible);
+        Log.i("AAAA", "setVisible");
     }
+
 }
--- app/src/main/java/com/zhouwei/anything/bean/Person.java
+++ app/src/main/java/com/zhouwei/anything/bean/Person.java
@@ -1,70 +1,25 @@
 package com.zhouwei.anything.bean;
 
-import android.content.ContentValues;
-
-import com.zhouwei.anything.xxorm.Sprite;
-
 /**
  * Created by zhouwei on 2017/12/14.
  */
 
-public class Person implements Sprite {
-    private int id;
-    private String name;
-    private int age;
+public class Person {
+    private int age = -2;
+    private float age1 = -2;
+    private double age2 = -2;
+    private long age3 = -2;
 
     public Person() {
-
-    }
-
-    public Person(ContentValues values) {
-        try {
-            setId(Integer.parseInt(String.valueOf(values.get("id"))));
-            setName(String.valueOf(values.get("name")));
-            setAge(Integer.parseInt(String.valueOf(values.get("age"))));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public ContentValues save() {
-        ContentValues values = new ContentValues();
-        values.put("name", name);
-        values.put("age", age);
-        return values;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 
     @Override
     public String toString() {
         return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
+                "age=" + age +
+                ", age1=" + age1 +
+                ", age2=" + age2 +
+                ", age3=" + age3 +
                 '}';
     }
 }
